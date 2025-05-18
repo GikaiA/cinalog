841e99e941deba1eb748dcea574f539ca2dabb6a
Gikai Andrews <gnaboys@gmail.com>
2025-05-18 13:50:51 -0400

    progressinf, added background to homepage

--- cinalog/src/Home/Home.jsx
+++ cinalog/src/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css';
+import cinemabg from '../images/cinema.jpg';
 
 function Home() {
   return (
     <div className='home'>
-        <div className='bg-wrapper'></div>
-      
+        <div className='bg-wrapper'>
+          <img src={cinemabg} alt='cinemabg' className='cinemabg'/>
+        </div>
+      <div className='hero-section'>
+        <h1 className='hero-title'>Welcome to Cinelog</h1>
+      </div>
     </div>
   )
 }
